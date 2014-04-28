475550affb97ec3664e79ffdd079932cef618b0d
altuz <altuz.nicholas@gmail.com>
2014-04-28 13:59:22 +0800

    Revert "Revert "Nicholas - Commit 1""
    This reverts commit 3bdf88501847fbef1b8e5f06470f8d3ba3035d09.

--- src/ese/gnomemsr2009/Controller.java
+++ src/ese/gnomemsr2009/Controller.java
@@ -6,7 +6,7 @@ public class Controller {
 
 	public static void main(String[] args) throws Exception
 	{
-	
+		
 		DatabaseAccessor da = new DatabaseAccessor();
 		IOFormatter io = new IOFormatter();
 		NetworkBuilder nb = new NetworkBuilder();
@@ -20,18 +20,33 @@ public class Controller {
 		{
 			System.out.println("Connected...");
 			System.out.println("");
-			io.inputData();
 			
-			long startTime = System.nanoTime();
-			da.sqlQueries(io.getProduct(), io.getStartDate(), io.getEndDate());
-			da.closeConnection();
-		
-			nb.networkBuilder(da.getDevelopers(), da.getDevelopers2(), da.getDevelopers3(), da.getEdges(), da.getNum());
+			int choice = io.inputChoice();
 			
-			io.writePajekFile(io.getProduct(), nb.getDCN());
-			long endTime = System.nanoTime();
+			if(choice == 1)
+			{
+				io.inputData();
+				
+				long startTime = System.nanoTime();
+				da.sqlQueries(io.getProduct(), io.getStartDate(), io.getEndDate());
+				da.closeConnection();
 			
-			System.out.println("Time Elapsed: " + ((endTime - startTime)/1000000) + " milliseconds");
+				nb.networkBuilder(da.getDevelopers(), da.getDevelopers2(), da.getDevelopers3(), da.getEdges(), da.getNum());
+				
+				io.writePajekFile(io.getProduct(), nb.getDCN());
+				long endTime = System.nanoTime();
+				
+				System.out.println("Time Elapsed: " + ((endTime - startTime)/1000000) + " milliseconds");
+			}else if(choice == 2)
+			{				
+				long startTime = System.nanoTime();
+				io.writeCSVFile(da.generateCSV());
+				long endTime = System.nanoTime();
+				System.out.println("Time Elapsed: " + ((endTime - startTime)/1000000) + " milliseconds");
+			}
+			
+			
+			
 		}
 		else
 		{
